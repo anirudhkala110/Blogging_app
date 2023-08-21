f126d09a1d5f7e68e6c841b8f6fa72edd2032415
anirudhkala110 <anirudhkala110@gmail.com>
2023-08-21 11:22:51 +0530

    updated

--- frontend/src/components/Routes/Create.jsx
+++ frontend/src/components/Routes/Create.jsx
@@ -6,11 +6,12 @@ const Create = () => {
     }
     return (
         <div>
-            <div className='container w-100 d-flex justify-content-center align-items-center border' style={{ height: "90vh" }}>
-                <form onSubmit={handleSubmit} className='border p-3'>
+            <div className='container w-100 d-flex justify-content-center align-items-center' style={{ height: "90vh" }}>
+                <form onSubmit={handleSubmit} className='border shadow p-3'>
                     <center className='fs-3 fw-bolder'>New Blog</center>
                     <div className='input-group'>
-                        <input type='text' className='form-control my-2' />
+                        <label className='form-label fw-bold text-center w-100 border-bottom border-dark p-2 text-uppercase bg-info rounded-top rounded-top-circle '>Title</label>
+                        <input type='text' placeholder='T i t l e' className='form-control my-2 w-100' />
                     </div>
                     <div className='input-group'>
                         <textarea cols='80' rows='10'></textarea>
